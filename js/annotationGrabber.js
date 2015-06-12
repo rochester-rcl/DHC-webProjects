ca25aded404baf74d9c5b1a925b1cc942f8af7f0
jjromphf <jromphf@library.rochester.edu>
2015-06-12 19:21:17 -0400

    more code - need to comment, refactor + add AJAX GET request on page load

--- js/annotationGrabber.js
+++ js/annotationGrabber.js
@@ -1,36 +1,3 @@
-//Global Functions
-
-//Set up a prototype for the String class to convert seconds (float) to HHMMSS format for display
-String.prototype.toHHMMSS = function() {
-    var sec_num = parseInt(this, 10);
-    var hours = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-    
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    
-     if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-    
-    var time = hours+":"+minutes+":"+seconds;
-    
-    return time;
-};
-
-
-function appendHTML(selector,text,start){
-    var appendTo = $(selector).append('<span href="#" id="sceneChangeMark"><span #id="sceneChangeText">' + text + '&nbsp' + start.toHHMMSS() + '</span><br /><span id="sceneChangeTimeSec">'+ start +'</span></span><br />');
-    return appendTo;
-    };
-
-
 // The Program
 $(document).ready( function() {
     
@@ -61,23 +28,21 @@ $timeLog.on("click", function (e) {
                                 text: $text + '&nbsp;' + '|' + '&nbsp',
                                 target: $target 
                             });
-                        
+                            
                             if ($markerType === 'Scene Change') {
-                         
-                                    appendHTML('#sceneChangeMarkers',$text,$start);
+                                    
+                                    appendHTML('.sceneChangeMarkers',$text,$start);
+                                    
+                   
+                                }
 
-                            }
+                      });
 
-                    });
+                    timeScrubbing('.sceneChangeMarkers','.sceneChangeMark');
+                    addPagination('.sceneChangeMark',25);
+
                     
-                    $('#sceneChangeMarkers').on('click', '#sceneChangeMark', function () {
-                                     
-                                       
-                                        var $timeSec = $(this).find('#sceneChangeTimeSec').text();
-                                        console.log($timeSec);
-                                        popcorn.currentTime($timeSec);
-                                        
-                                    });
+                    
                 }
             },
         error: function(xhr, desc, err) {
@@ -86,5 +51,10 @@ $timeLog.on("click", function (e) {
         }
     });
     e.preventDefault();
+                                      
+                                            
 });
+
+
+                                    
 });
